478f1b67656c2fccaf85bcb5bb4adcb5fdee5343
Yure Plácido <yurekesley@gmail.com>
2026-09-20 16:14:37 -0300

    refactor(reservation): isolate rental REST adapter

---
+++ reservation-service/src/main/java/org/acme/reservation/adapter/out/rental/RentalClient.java
@@ -0,0 +1,16 @@
+package org.acme.reservation.adapter.out.rental;
+
+import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.reactive.RestPath;
+
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+@Path("/rental")
+@RegisterRestClient(configKey = "rental")
+public interface RentalClient {
+    @POST
+    @Path("/start/{userId}/{reservationId}")
+    Uni<RentalResponse> start(@RestPath String userId, @RestPath Long reservationId);
+}
