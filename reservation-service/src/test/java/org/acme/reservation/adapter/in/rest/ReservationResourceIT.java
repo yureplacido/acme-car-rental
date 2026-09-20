62db36f8fcd903ac7bcfcbc851219cd5b19eabf7
Yure Plácido <yurekesley@gmail.com>
2026-09-20 16:18:53 -0300

    test(reservation): rebuild tests around domain and adapters

---
+++ reservation-service/src/test/java/org/acme/reservation/adapter/in/rest/ReservationResourceIT.java
@@ -0,0 +1,7 @@
+package org.acme.reservation.adapter.in.rest;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ReservationResourceIT extends ReservationResourceTest {
+}
