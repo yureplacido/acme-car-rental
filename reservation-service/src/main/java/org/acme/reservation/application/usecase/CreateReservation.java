d39cebf40e96ca05daf61e05e77c7fc4ee720ca5
Yure Plácido <yurekesley@gmail.com>
2026-09-20 16:32:47 -0300

    refactor(reservation): keep transaction concerns out of application layer

--- reservation-service/src/main/java/org/acme/reservation/application/usecase/CreateReservation.java
+++ reservation-service/src/main/java/org/acme/reservation/application/usecase/CreateReservation.java
@@ -1,7 +1,6 @@
 package org.acme.reservation.application.usecase;
 
 import io.smallrye.mutiny.Uni;
-import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.acme.reservation.application.port.out.RentalGateway;
@@ -26,7 +25,6 @@ public class CreateReservation {
         this.rentalGateway = rentalGateway;
     }
 
-    @WithTransaction
     public Uni<Reservation> handle(Command command) {
         Reservation reservation = Reservation.create(
                 new CustomerId(command.customerId()),
